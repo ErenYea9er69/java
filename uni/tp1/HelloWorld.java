453aba1c3a7b07d46014d7b79b32efda7898dbfb
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-09-27 19:08:39 +0100

    uni exe 1 methd

--- uni/tp1/HelloWorld.java
+++ uni/tp1/HelloWorld.java
@@ -1,7 +1,14 @@
 package uni.tp1;
 
+//public class HelloWorld {
+//    public static void main(String[] args) {
+     //   System.out.println("Hello World java");
+   // }
+//}
+
+//metho2:
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World java");
+        System.out.println(args[0]);
     }
 }
