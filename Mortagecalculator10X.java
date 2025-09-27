73622fa6ac396c765d824467c2f2abcef0ff1076
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-09-27 16:00:06 +0100

    Mortage calculator with loops

---
+++ Mortagecalculator10X.java
@@ -0,0 +1,53 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Mortagecalculator10X {
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        int principal;
+        float annualInterest;
+        byte years;
+
+        // Principal validation loop
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1,000 and 1,000,000");
+        }
+
+        // Annual Interest validation loop
+        while (true) {
+            System.out.print("Annual Interest Rate (1-30): ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30)
+                break;
+            System.out.println("Enter a value between 1 and 30");
+        }
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        // Period validation loop
+        while (true) {
+            System.out.print("Period (Years) (1-30): ");
+            years = scanner.nextByte();
+            if (years >= 1 && years <= 30)
+                break;
+            System.out.println("Enter a value between 1 and 30");
+        }
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+
+        scanner.close();
+    }
+}
