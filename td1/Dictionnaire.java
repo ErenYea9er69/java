6af01a7ba7b5d6614bade0f2b6504c8ac4392b97
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-10-08 11:29:49 +0100

    td1 exe9 e

--- td1/Dictionnaire.java
+++ td1/Dictionnaire.java
@@ -1,6 +1,7 @@
 package td1;
 
 
+
 public class Dictionnaire {
 
     private int nb_mots;
@@ -13,88 +14,106 @@ public class Dictionnaire {
         this.Dict = new Mot_dict[nb_mots];
     }
 
-    public void Ajouter_mot(Mot_dict m) {
-        if (nb_mots == 0) {
-            Dict[0] = m;
-            nb_mots++;
-        } else {
-            if (nb_mots < Dict.length) {
-                Dict[nb_mots] = m;
-                nb_mots++;
-                Trier();
-            }
-        }
-    }
+    public void Ajouter_mot(Mot_dict m){
+        if (nb_mots < 0){
+            Dict[0]=m;
 
-    public void Trier() {
-        for (int i = 0; i < nb_mots - 1; i++) {
-            for (int j = i + 1; j < nb_mots; j++) {
-                if (Dict[i].getMot().compareTo(Dict[j].getMot()) > 0) {
-                    Mot_dict t = Dict[i];
-                    Dict[i] = Dict[j];
-                    Dict[j] = t;
+        }
+        else{
+            for (int i=0; i<nb_mots;i++){
+                if (Dict[i].getMot().compareTo(m.getMot())==1){
+
+                    for (int j=nb_mots;j>i;j--){
+                        Dict[j]=Dict[j-1];
+                    }
+                    Dict[i]=m;
+                    nb_mots++;
+                    break;
                 }
+                    
             }
         }
 
     }
+    
 
-    public void Supprimer_mot(Mot_dict a) {
-        for (int i = 0; i < nb_mots; i++) {
 
-            if (Dict[i].synonyme(a.getMot())) {
-                Dict[i] = null;
-                nb_mots--;
-                break;
 
+public void Trier(){
+    for (int i=0; i<nb_mots-1;i++){
+        for (int j=i+1; j<nb_mots;j++){
+            if (Dict[i].getMot().compareTo(Dict[j].getMot())>0){
+                Mot_dict t=Dict[i];
+                Dict[i]=Dict[j];
+                Dict[j]=t;
             }
+        }
+    }
+
+
+
+}
+
+public void Supprimer_mot(Mot_dict a){
+    for (int i=0; i<nb_mots;i++){
+
+        if (Dict[i].synonyme(a.getMot()   )){
+            Dict[i]=null;
+            nb_mots--;
+            break;
+            
+        }
+    }
+
+
+}
+
+public String Recherche_dicho(String mot){
+
+    int i=0;
+    int n=nb_mots-1;
+    String s="";
+    boolean t=false;
+    while (n!=0 && t==false){
+        if (Dict[i].getMot().equals(mot)){
+            s=  Dict[i].getDefinition();
+            t=true;
+        }
+        else{
+            n--;
+            i++;
+        }
+        
+
+    }
+    if (t==false){
+        s="no mot";
+    }
+    return s;
+
+}
+
+public void Lister_dictionnaire(){
+    for (int i=0; i<Dict.length;i++){
+        if (Dict[i]!=null){
+            System.out.println(Dict[i].toString());
         }
 
     }
 
-    public String Recherche_dicho(String mot) {
+}
 
-        int i = 0;
-        int n = nb_mots - 1;
-        String s = "";
-        boolean t = false;
-        while (n != 0 && t == false) {
-            if (Dict[i].getMot().equals(mot)) {
-                s = Dict[i].getDefinition();
-                t = true;
-            } else {
-                n--;
-                i++;
-            }
-
+public int Nombre_synonymes(Mot_dict a){
+    int s=0;
+    for (int i=0; i<nb_mots;i++){
+        if (Dict[i].synonyme(a.getMot()   )){
+            s++;
         }
-        if (t == false) {
-            s = "no mot";
-        }
-        return s;
-
     }
+    return s;
+        
 
-    public void Lister_dictionnaire() {
-        for (int i = 0; i < Dict.length; i++) {
-            if (Dict[i] != null) {
-                System.out.println(Dict[i].toString());
-            }
-
-        }
-
-    }
-
-    public int Nombre_synonymes(Mot_dict a) {
-        int s = 0;
-        for (int i = 0; i < nb_mots; i++) {
-            if (Dict[i].synonyme(a.getMot())) {
-                s++;
-            }
-        }
-        return s;
-
-    }
+}
 
 }
 
--- td1/Main.java
+++ td1/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Dictionnaire d = new Dictionnaire("dictionnaire", 3);
         Mot_dict m1 = new Mot_dict("salem", "3aslema");
         Mot_dict m2 = new Mot_dict("rise", "9oum 3al sbe7");
-        Mot_dict m3 = new Mot_dict("bed", "win nor9dou");
+        Mot_dict m3 = new Mot_dict("cat", "9atous im3we9");
         d.Ajouter_mot(m1);
 
         d.Ajouter_mot(m2);
@@ -18,6 +18,8 @@ public class Main {
 
         d.Supprimer_mot(m2);
         d.Lister_dictionnaire();    
+
+        d.Nombre_synonymes(m1);
 
     }
     
